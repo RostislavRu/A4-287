0c9ad3a5981bc69476fed74c8f2eb3d73b60fee8
Rostislav Rusev <93301833+RostislavRu@users.noreply.github.com>
2022-08-07 18:55:37 -0400

    Update Q2.php

--- Q2.php
+++ Q2.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
 setcookie("lastVisit", date("Y-m-d H:i:s"),time()+60*60*24*365); 
-?>
-<html> 
-  <head> 
-    <title>PHP Code to Count Number of Visitors Using Cookies: PHPCODER</title> 
- </head> 
-<body> 
-<?php 
-
 if (!isset($_COOKIE['count'])) { 
   echo "Welcome! This is the first time you have viewed this page."; 
   $cookie = 1;
@@ -20,8 +12,10 @@ if (!isset($_COOKIE['count'])) {
   $lastVisit = "<p>Your last visit was on ".$_COOKIE['lastVisit'];
   echo "You have viewed this page ".$_COOKIE['count']." times."; 
   echo $lastVisit;
-} 
-
-?> 
-  </body> 
+}
+?>
+<html> 
+  <head> 
+    <title>PHP Code to Count Number of Visitors Using Cookies: PHPCODER</title> 
+ </head> 
 </html>
