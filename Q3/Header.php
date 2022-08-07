6006764545d02223a4e9d2192d63296be9043fb3
Rostislav Rusev <93301833+RostislavRu@users.noreply.github.com>
2022-08-07 18:41:21 -0400

    Delete Q3 directory

--- Q3/Header.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <table><tr><td>Concordia Pet Adoption Center</td><td><a href="Home.php"><img src="Cat & Dog 3.png" alt="cat and dog icon" height="100" width="100"></a></td></tr></table>
-    <div id="todaysDate" onfocus="doDate()"></div>
-</header>
