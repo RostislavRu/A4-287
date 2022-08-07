3f038e2f698e087939c544b4b12a92dc3096bebc
Rostislav Rusev <93301833+RostislavRu@users.noreply.github.com>
2022-08-07 18:49:55 -0400

    Update FindPet.php

--- Q3/FindPet.php
+++ Q3/FindPet.php
@@ -1,6 +1,27 @@
 <?php
-    require_once 'functions.inc.php';
     session_start();
+
+    function matchPets($type, $breed, $age, $gender, $along){
+        $file = fopen('availablePets.txt', 'r') or die("Can't open file");
+        echo '<div class="grid-container">';
+
+        while(!feof($file)){
+            $line = fgets($file);
+            $arrFile = explode(':', $line);
+            $arrAge = explode('-', $age);
+
+            if($type == $arrFile[2] && ($arrAge[0] <= $arrFile[4] && $arrFile[4] <= $arrAge[1]) && ($gender == "none/" || $gender == $arrFile[6])){
+                if($along == '' || strpos($arrFile[7], $along) !== false){
+                    if($breed == "none"){
+                        echo '<div class="grid-item">'.$arrFile[4].' year old '.$arrFile[5].' '.str_replace('/', '', $arrFile[2]).', '.str_replace('/', '', $arrFile[6]).'.</br>Gets along with '.$arrFile[7].'.</br>Extra details: '.$arrFile[8].'</br><input type="button" value="Interested"></div>';
+                    }else if(strpos($breed, $arrFile[5]) !== false){
+                        echo '<div class="grid-item">'.$arrFile[4].' year old '.$arrFile[5].' '.str_replace('/', '', $arrFile[2]).', '.str_replace('/', '', $arrFile[6]).'.</br>Gets along with '.$arrFile[7].'.</br>Extra details: '.$arrFile[8].'</br><input type="button" value="Interested"></div>';
+                    }
+                }
+            }
+        }
+        echo '</div>';
+    }
 ?>
 <!DOCTYPE html>
 <html  lang="en"> 
