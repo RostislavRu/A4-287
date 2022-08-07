155d3024e9e5ef0e39de96be1867fa9c7c0996d6
Rostislav Rusev <93301833+RostislavRu@users.noreply.github.com>
2022-08-07 17:18:15 -0400

    fr

---
+++ A4-287/Q3/Footer.php
@@ -0,0 +1,3 @@
+<footer>
+        <a href="javascript:alert('Any information submitted will not be sold or misused. We are not liable for any wrong information in the BrowsePet section. We do not own the images displayed, their source can be found by clicking on them.');">Privacy/Disclaimer Statement</a>
+    </footer>
---
+++ A4-287/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html> 
+    <head> 
+        <title>A4 Home Page</title> 
+    </head> 
+    <body text-align:center>
+        <a href="Q1.php">Exercise 1</a></br></br>
+        <a href="Q2.php">Exercise 2</a></br></br>
+        <a href="Q3/Home.php">Exercise 3</a></br></br>
+    </body>
+</html>
